1cb01dede25a374a70dc72c6935b8a7d61c5079e
ricklarios <81334604+ricklarios@users.noreply.github.com>
2022-06-08 23:48:29 +0200

    Initial Commit

---
+++ CarFactory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarFactory
+//
+//  Created by Rick Larios on 8/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
